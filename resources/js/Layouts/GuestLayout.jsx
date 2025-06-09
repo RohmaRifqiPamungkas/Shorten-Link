cb73a576ccd4d739fdcc33ddc8dd2181fbb886e7
RohmaRifqiPamungkas <rohmarifqi31@gmail.com>
2025-06-09 11:10:37 +0700

    fix : justify content in auth and fix placeholder

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,10 +3,10 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0 bg-background">
-        <div className="mt-6 w-full overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-2xl bg-background shadow-primary">
-            {children}
+        <div className="flex min-h-screen flex-col items-center justify-center py-6 sm:justify-center px-10 sm:pt-10 bg-background">
+            <div className="mt-6 w-full overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-2xl bg-background shadow-primary">
+                {children}
+            </div>
         </div>
-    </div>
     );
 }
