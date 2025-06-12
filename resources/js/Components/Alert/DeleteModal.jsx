ccf1b2bbfb6c51456132d9f433572afaeda21ebd
Ameliadaa <ameliadia722@gmail.com>
2025-06-12 15:13:13 +0700

    Fix layout responsive
    modal create categories
    modal cretae project
    modal delete
    modal share
    guestlayout

--- resources/js/Components/Alert/DeleteModal.jsx
+++ resources/js/Components/Alert/DeleteModal.jsx
@@ -4,24 +4,24 @@ const DeleteModal = ({ isOpen, onClose, onConfirm, processing }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-5">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-sm md:max-w-2xl flex flex-col items-center justify-center py-20 px-10">
         <div className=" w-20 md:w-[150px] flex justify-center mb-4">
           <img src="/images/info-circle.png" alt="info" />
         </div>
-        <h2 className="text-lg md:text-2xl font-semibold mb-4">Delete this link?</h2>
-        <p className="mb-6 text-sm md:text-lg text-gray-600 text-center">All data related to the link will be completely deleted. This action cannot be undone.</p>
-        <div className="flex justify-end gap-6">
+        <h2 className="text-lg md:text-2xl font-semibold mb-4">Delete this item?</h2>
+        <p className="mb-6 text-sm md:text-lg text-gray-600 text-center">This action is permanent and cannot be undone. Please confirm your choice.</p>
+        <div className="flex justify-end gap-2 md:gap-6">
           <button
             onClick={onConfirm}
-            className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-secondary"
+            className="px-6 py-2 text-sm md:text-lg bg-red-600 text-white rounded-lg hover:bg-secondary"
             disabled={processing}
           >
             {processing ? "Deleting..." : "Delete"}
           </button>
           <button
             onClick={onClose}
-            className="px-6 py-2 border border-foreground rounded-lg hover:bg-gray-400 hover:border-none hover:text-white"
+            className="px-6 py-2 text-sm md:text-lg border border-foreground rounded-lg hover:bg-gray-400 hover:border-none hover:text-white"
             disabled={processing}
           >
             Cancel
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/react';
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-6 sm:justify-center px-10 sm:pt-10 bg-background">
-            <div className="mt-6 w-full overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-2xl bg-background shadow-primary">
+            <div className="mt-6 w-full overflow-hidden px-6 py-4 sm:max-w-md rounded-2xl bg-background shadow-primary">
                 {children}
             </div>
         </div>
