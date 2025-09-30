c1953b4373ad4052c140b574018865d19863665a
Pamungkas <rohmarifqi31@gmail.com>
2025-09-30 13:32:31 +0700

    [FIX] build in the public assets

---
+++ public/build/assets/InputLabel-2LWM7AiK.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-CL3xyi0u.js";function s({value:t,className:e="",children:o,...r}){return m.jsx("label",{...r,className:"block text-sm md:text-lg font-medium text-foreground  "+e,children:t||o})}export{s as I};
---
+++ public/build/assets/index-Q2p4lVjq.js
@@ -0,0 +1,1 @@
+import{d as r}from"./app-CL3xyi0u.js";var a=r();export{a as r};
